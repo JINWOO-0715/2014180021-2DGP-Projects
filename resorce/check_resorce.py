3623ab9a9143b198c60b757cfb67837abe45eece
PJW_94 <archon1480@kpu.ac.kr>
2019-10-11 00:52:54 +0900

    주인공리소스
    움직임 확인 완료

---
+++ resorce/check_resorce.py
@@ -0,0 +1,38 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('curbe.png')
+speed = 10
+def Move_right():
+    x,y = 0+25,90
+    frame=0
+    while (x < 800-25):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 35, 0, 35, 50, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 6
+        x += speed
+        delay(0.05)
+        get_events()
+  
+def Move_left():
+    x,y =800-25,90
+    frame=0
+    while(x>0+25):
+        clear_canvas()
+        grass.draw(400,30)
+        character.clip_draw(frame*35,0,35,50,x,90)
+        update_canvas()
+        frame = (frame+1)%6
+        x -= speed
+        delay(0.05)
+        get_events()
+
+while True:
+    Move_right()
+    Move_left()
+
+close_canvas()
+        
+        

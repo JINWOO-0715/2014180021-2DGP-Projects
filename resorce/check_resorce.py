125dbe2cedf82f453bb3819aa0106280fe0a5b32
PJW_94 <pjw1480@naver.com>
2019-10-11 11:53:46 +0900

    파워에 따른 총알 리소스 추가
    확인완료

--- resorce/check_resorce.py
+++ resorce/check_resorce.py
@@ -1,7 +1,7 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('bullet_1.png')
+character = load_image('bullet_level_2.png')
 speed = 10
 def Move_right():
     x,y = 0+25,90
@@ -9,7 +9,7 @@ def Move_right():
     while (x < 800-25):
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame * 11, 0, 11, 11, x, 90)
+        character.clip_draw(frame * 21, 0, 21, 15, x, 90)
         update_canvas()
         frame = (frame + 1) % 1
         x += speed

ba269471cbad4cc2b7bcbd04b3a563f6b67c231a
PJW_94 <pjw1480@naver.com>
2019-10-11 10:54:06 +0900

    총알 리소스
    총알 리소스 추가

--- resorce/check_resorce.py
+++ resorce/check_resorce.py
@@ -1,7 +1,7 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('curbe.png')
+character = load_image('bullet_1.png')
 speed = 10
 def Move_right():
     x,y = 0+25,90
@@ -9,9 +9,9 @@ def Move_right():
     while (x < 800-25):
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame * 35, 0, 35, 50, x, 90)
+        character.clip_draw(frame * 11, 0, 11, 11, x, 90)
         update_canvas()
-        frame = (frame + 1) % 6
+        frame = (frame + 1) % 1
         x += speed
         delay(0.05)
         get_events()
